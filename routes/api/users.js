e3b375f28620857ad6ef1a3072de0a68c89ad094
Marc Hulterström <marc.hulterstrom@gmail.com>
2021-08-20 17:52:04 +0200

    Creating router and api endpoints

---
+++ routes/api/users.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+
+
+// @route     GET api/users
+// @desc      Test route
+// @access    Public
+
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('users you')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
  const express = require('express')
+ const connectDB = require('./config/db.js')
 
  const app = express()
 
+app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => {
-  console.log("RUNNUING")
-  res.send('KÖÖRRRR!!!!')
-})
+ connectDB()
+
+ app.get('/', (req, res) => {
+   console.log("RUNNUING")
+   res.send('KÖÖRRRR!!!!')
+ })
+
+ app.use('/api/users', require('./routes/api/users'))
+ app.use('/api/auth', require('./routes/api/auth'))
+ app.use('/api/profile', require('./routes/api/profile'))
+ app.use('/api/posts', require('./routes/api/posts'))
 
  const PORT = process.env.PORT || 5000
-
  app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
